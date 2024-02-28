dcc9f39fb169268e6167ccb897b7fbb24e7739eb
anisashafira <anisashafira07@gmail.com>
2024-02-28 11:12:38 +0700

    W03: Jawaban soal 1

---
+++ 03-belajar-komponen/my-app/src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Profile from "../components/profile";
+
+export default function Home() {
+  return (
+    <div>
+    <h1 className="text-center font-bold">Ilmuwan yang luar biasa</h1>
+    <div className="grid grid-cols-3 gap-8 justify-center">
+      <Profile></Profile>
+      <Profile></Profile>
+      <Profile></Profile>
+    </div>
+    </div>
+  );
+}
