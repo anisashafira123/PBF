2ee21c0bdb3be15a50aca81b36714e9a046fdb40
anisashafira <anisashafira07@gmail.com>
2024-02-29 13:30:23 +0700

    W03: Jawaban soal 5

--- 03-belajar-komponen/my-app/src/app/page.tsx
+++ 03-belajar-komponen/my-app/src/app/page.tsx
@@ -1,29 +1,41 @@
 import Image from "next/image";
 import Profile from "../components/profile";
-
-
-// export default function Home() {
-//   return (
-// //     <div>
-// //     <h1 className="text-center font-bold">Ilmuwan yang luar biasa</h1>
-// //     <div className="grid grid-cols-3 gap-8 justify-center">
-// //       <Profile></Profile>
-// //       <Profile></Profile>
-// //       <Profile></Profile>
-// //     </div>
-// //     </div>
-// //   );
-// // }
-
 import { Gallery } from "@/components/gallery";
-import Bio from "@/components/bio";
+import TodoList from "@/components/todolist";
 
 export default function Home() {
   return (
-    <div>
-      <div className="grid grid-cols-3 gap-8 justify-center">
-        <Bio />
-      </div>
-    </div>
-  )
+    <section>
+      <h1 className="font-semibold text-slate-900 truncate pr-20 text-center">Ilmuwan yang luar biasa</h1>
+      <hr />
+      <Gallery />
+      <hr />
+      <TodoList />
+    </section>
+  );
 }
+// export default function Home() {
+//   return (
+//     <div>
+//     <h1 className="text-center font-bold">Ilmuwan yang luar biasa</h1>
+//     <div className="grid grid-cols-3 gap-8 justify-center">
+//       <Profile></Profile>
+//       <Profile></Profile>
+//       <Profile></Profile>
+//     </div>
+//     </div>
+//   );
+// }
+
+
+// import Bio from "@/components/bio";
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <div className="grid grid-cols-3 gap-8 justify-center">
+//         <Bio />
+//       </div>
+//     </div>
+//   )
+// }
