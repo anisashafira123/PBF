846b1a149369c1c13705e60cbb5419ba6e39a376
anisashafira <anisashafira07@gmail.com>
2024-02-29 14:30:05 +0700

    W03: Jawaban soal 7

--- 03-belajar-komponen/my-app/src/app/page.tsx
+++ 03-belajar-komponen/my-app/src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Profile from "../components/profile";
 import { Gallery } from "@/components/gallery";
 import TodoList from "@/components/todolist";
+import MyGallery from "@/components/mygallery"
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Gallery />
       <hr />
       <TodoList />
+      <MyGallery/>
     </section>
   );
 }
---
+++ 03-belajar-komponen/my-app/src/utils/utils.tsx
@@ -0,0 +1,16 @@
+export function getImageUrl(imageId:string, size = 's') {
+    return (
+      'https://i.imgur.com/' +
+      imageId +
+      size +
+      '.jpg'
+    );
+  }
+  export function getImageUrlV2(person: { imageId: string; }, size: 's') {
+    return (
+      'https://i.imgur.com/' +
+      person.imageId +
+      size +
+      '.jpg'
+    );
+  }
