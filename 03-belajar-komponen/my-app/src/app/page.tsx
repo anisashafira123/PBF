a8bcf88443a00575aa23608dfa4dc69732d23e1e
anisashafira <anisashafira07@gmail.com>
2024-02-29 13:13:41 +0700

    W03: Jawaban soal 3

--- 03-belajar-komponen/my-app/src/app/page.tsx
+++ 03-belajar-komponen/my-app/src/app/page.tsx
@@ -1,17 +1,29 @@
 import Image from "next/image";
 import Profile from "../components/profile";
 
+
+// export default function Home() {
+//   return (
+// //     <div>
+// //     <h1 className="text-center font-bold">Ilmuwan yang luar biasa</h1>
+// //     <div className="grid grid-cols-3 gap-8 justify-center">
+// //       <Profile></Profile>
+// //       <Profile></Profile>
+// //       <Profile></Profile>
+// //     </div>
+// //     </div>
+// //   );
+// // }
+
+import { Gallery } from "@/components/gallery";
+import Bio from "@/components/bio";
+
 export default function Home() {
   return (
     <div>
-    <h1 className="text-center font-bold">Ilmuwan yang luar biasa</h1>
-    <div className="grid grid-cols-3 gap-8 justify-center">
-      <Profile></Profile>
-      <Profile></Profile>
-      <Profile></Profile>
+      <div className="grid grid-cols-3 gap-8 justify-center">
+        <Bio />
+      </div>
     </div>
-    </div>
-  );
+  )
 }
-
-
---
+++ 03-belajar-komponen/my-app/src/components/bio.jsx
@@ -0,0 +1,12 @@
+export default function Bio() {
+    return (
+      <div className="intro">
+        <h1>Selamat datang di website saya!</h1>
+        <p className="summary">
+          Anda dapat membaca uneg-unegku di sini.
+          <br />
+          <b>Juga ada <i>Foto</i></b>
+        </p>
+      </div>
+    );
+  }
