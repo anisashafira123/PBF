14819eaa13ca23bfb4e91f69f1a1b1554c804fb4
anisashafira <anisashafira07@gmail.com>
2024-03-06 11:29:03 +0700

    Praktikum 4

---
+++ 04-event-state/src/app/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+import Tombol_1, {Tombol_2, Tombol_3} from "@/components/button";
+import Gallery from "@/components/gallery";
+
+export default function Home() {
+  return (
+    <>
+      <div className="container mx-auto">
+        <h2>Kuis Kota</h2>
+      <Tombol_1/>
+      <hr></hr>
+      <Tombol_2 isiPesan="Ini Pesanku" namaTombol="Pesan" />
+    </div>
+    <br></br>
+    <div className="bg-red-300" onClick={() => alert('Parent Element : Div')}>
+      <Tombol_3 isiPesan="Child Element : Tombol-1" namaTombol="Tombol-1"/>
+      <Tombol_3 isiPesan="Child Element : Tombol-2" namaTombol="Tombol-2"/>
+    </div>
+    <Gallery/>
+    </>
+  );
+}
