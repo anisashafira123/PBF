64973158203d8bdbe73bcee857d01f23b0368864
anisashafira <anisashafira07@gmail.com>
2024-03-14 23:27:04 +0700

    Praktikum 1

---
+++ routing-demo-main/src/app/about/profile.tsx
@@ -0,0 +1,12 @@
+export default function Profile() {
+    return (
+        <div>
+        <h1>Profil</h1>
+        <p><strong>Nama: Anisa Shafira</strong></p>
+        <p><strong>Umur: 23</strong> </p>
+        <p><strong>Pekerjaan: Mahasiswa</strong> </p>
+        <p><strong>Email: anisashafira07@gmail.com</strong> </p>
+        <p><strong>Lokasi: Malang</strong></p>
+      </div>
+    ) 
+    }
---
+++ routing-demo-main/src/app/page.tsx
@@ -0,0 +1,12 @@
+// import About from "./about/page"
+// import Profile from "./about/profile"
+
+export default function Home() {
+    return (
+        <>
+    <h1>Welcome to Home</h1>
+    {/* <About/>
+    <Profile/> */}
+        </>
+    )
+}
